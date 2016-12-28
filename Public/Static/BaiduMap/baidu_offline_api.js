4cde4936a0c18a9eb94eb2645420d7b5d453d161
難嘚煳凃 <184117183@qq.com>
2016-12-28 14:26:10 +0800

    更新插件、替换无用插件

--- Public/Static/BaiduMap/baidu_offline_api.js
+++
@@ -1,1 +0,0 @@
-(function(){window.wise=1;window.netSpeed=341;window.netType=1; window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="./Public/Static/BaiduMap/getscript_baidu.js"></script>');})();
